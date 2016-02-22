b6de1deb823c23f832bd1a2fec278baec66596f0
Brenka <brenka@MacMIni.local>
2016-02-21 18:46:32 -0600

    Initial Commit

---
+++ Swift Week 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Week 4
+//
+//  Created by Brenka on 21/02/16.
+//  Copyright (c) 2016 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
